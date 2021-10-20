d1aebed2be8727382b4974c95e05a08e55855fcc
jordanx <ferlzjordan@hotmail.com>
2021-10-19 20:38:00 -0400

    first push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'main', component: MainComponent, children:[
+    {path: 'home', component: HomeComponent}]},
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo:"login" , pathMatch:"prefix"}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
